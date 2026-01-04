3cd0c6af0ef8d377600ec289d31b829cb880fda7
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-01-04 14:57:28 +0200

    Solution for Department Highest Salary

---
+++ Question_184.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select D.name as Department ,E.name as Employee, E.salary as Salary
+from Employee E,Department D
+where E.departmentId = D.id
+and E.salary = (
+    select max(salary)
+    from Employee
+    where departmentId = E.departmentId)
