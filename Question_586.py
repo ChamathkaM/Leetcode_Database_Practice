08f3763f4bbd06a3e631f35b866f83fb01faee84
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-01-11 21:08:22 +0200

    Solution for Customer Placing the Largest Number of Orders

---
+++ Question_586.py
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with cte_num_of_orders as 
+(select customer_number,count(order_number) as num_of_orders
+from Orders
+group by customer_number)
+
+
+select customer_number from cte_num_of_orders 
+where num_of_orders=(select max(num_of_orders) from cte_num_of_orders) 
