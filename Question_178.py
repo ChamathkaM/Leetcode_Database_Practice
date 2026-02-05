7a83fecea6a38a961fb90fd22ec3ac9979c4065f
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-02-05 20:12:43 +0200

    different solution for Question_178.py

--- Question_178.py
+++ Question_178.py
@@ -1,2 +1,8 @@
-select score, DENSE_RANK() over(order by score desc) as 'rank' # solution for rank scores 
-from Scores;
+# select score, DENSE_RANK() over(order by score desc) as 'rank' # solution for rank scores 
+# from Scores;
+
+SELECT S.Score, COUNT(S2.Score) AS Rank FROM Scores S,
+(SELECT DISTINCT Score FROM Scores) S2
+WHERE S.Score<=S2.Score
+GROUP BY S.Id 
+ORDER BY S.Score DESC;
