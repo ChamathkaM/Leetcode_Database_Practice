5995b9dcc89405967a921d5a9d2b1f9a7ebfc521
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-02-06 14:40:01 +0200

    Update Question_180.sql

--- Question_180.sql
+++ Question_180.sql
@@ -1,4 +1,4 @@
-select distinct l1.num as ConsecutiveNums 
+select distinct l1.num as ConsecutiveNums #consecutive numbers
 from Logs l1, Logs l2, Logs l3
 where l1.num=l2.num and l2.num=l3.num
 and l1.id=l2.id-1 and l2.id=l3.id-1
