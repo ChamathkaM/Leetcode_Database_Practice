729dc7bcd0ac48e104ad3e5f2702d866eaabb8fc
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-02-09 15:18:01 +0200

    Solution for Classes With at Least 5 Students

--- Question_596.sql
+++ Question_596.sql
@@ -1,1 +1,9 @@
-
+WITH class_counts AS (
+    SELECT 
+        class,
+        COUNT(*) OVER (PARTITION BY class) as student_count
+    FROM Courses
+)
+SELECT DISTINCT class
+FROM class_counts
+WHERE student_count >= 5;
