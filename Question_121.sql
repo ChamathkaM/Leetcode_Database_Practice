a9b679cc2fcc4d7292e7e32d0bd35393ca6592a9
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-03-13 20:29:55 +0200

    Update Question_121.sql

--- Question_121.sql
+++ Question_121.sql
@@ -1,1 +1,11 @@
 --solution for Best Time to Buy and Sell Stock
+class Solution:
+    def maxProfit(self, prices):
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
