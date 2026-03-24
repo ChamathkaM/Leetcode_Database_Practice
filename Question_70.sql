4593ac209839a8c28a0beab6c5a2542a60f6a9fc
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-03-24 16:53:31 +0200

    Update Question_70.sql

--- Question_70.sql
+++ Question_70.sql
@@ -1,1 +1,13 @@
 --solution for climbing stairs
+class Solution(object):
+    def climbStairs(self, n):
+        prev = 1
+        prev2 = 0
+        for i in range(1, n+1):
+            curi = prev + prev2
+            prev2 = prev
+            prev = curi
+        return prev 
+
+
+        
