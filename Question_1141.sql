ba7a872f971ebadb2dd6d2699723bb3da722c8b3
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-04-13 21:51:46 +0300

    Update Question_1141.sql

--- Question_1141.sql
+++ Question_1141.sql
@@ -1,1 +1,6 @@
 #solution for User Activity for the Past 30 Days I
+
+select activity_date as day ,count(DISTINCT user_id) as active_users 
+from Activity
+where activity_date > "2019-06-27" AND activity_date <= "2019-07-27"
+group by activity_date
