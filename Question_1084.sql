743aaa347a757b7da4ca65272f67bec12454087a
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-05-15 10:39:33 +0300

    Update Question_1084.sql

--- Question_1084.sql
+++ Question_1084.sql
@@ -1,1 +1,8 @@
-
+#first solution for question sales analysis III
+select distinct p.product_id, p.product_name
+from Product p,  
+Sales s
+where p.product_id=s.product_id
+and s.Sale_date between '2019-01-01' and '2019-03-31'
+and p.product_id not in (select product_id from Sales
+where Sale_date not between '2019-01-01' and '2019-03-31')
