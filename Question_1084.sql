3921131c734183f6a9309e61c72d4b386f7bc63d
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2026-05-19 22:04:32 +0300

    Update Question_1084.sql

--- Question_1084.sql
+++ Question_1084.sql
@@ -3,7 +3,7 @@ select distinct p.product_id, p.product_name ###
 from Product p,  
 Sales s
 where p.product_id=s.product_id
-and s.Sale_date between '2019-01-01' and '2019-03-31'
+and s.Sale_date between '2019-01-01' and '2019-03-31'----
 and p.product_id not in (select product_id from Sales
 where Sale_date not between '2019-01-01' and '2019-03-31')
 
